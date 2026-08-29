ba4debb53ddcb96e886f9d64237a8123f17321de
ramhomelabs-art <ramhomelabs@gmail.com>
2026-08-29 14:27:23 +0530

    fix: import useMemo and useEffect in ClusterCockpitView

--- client/src/components/views/ClusterCockpitView.jsx
+++ client/src/components/views/ClusterCockpitView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { 
     Cpu, Server, HardDrive, Globe, Activity, RefreshCw, 
     ShieldCheck, Zap, Radio, ArrowUpRight, ArrowDownRight, 
